6408f8b3e5c63c7cd7ef8f9e47d3a2e077becab4
Justin Lee <justin.adam.lee@gmail.com>
2024-11-22 15:19:59 -0500

    finish generator

---
+++ src/generator.rs
@@ -0,0 +1,154 @@
+use core::fmt;
+use std::{collections::HashSet, hash};
+
+use fraction::Fraction;
+use rand::Rng;
+
+const MAX_CARD_VALUE: u64 = 15;
+const NUM_CARDS: u64 = 4;
+const TARGET_NUM: u64 = 24;
+
+#[derive(Clone, Debug)]
+pub enum NumericExpression {
+    Number(u64),
+    Expression {
+        num_1: Box<NumericExpression>,
+        num_2: Box<NumericExpression>,
+        operator: &'static str,
+    },
+}
+
+impl NumericExpression {
+    fn _eval(&self) -> Fraction {
+        match self {
+            &NumericExpression::Number(n) => n.into(),
+            NumericExpression::Expression {
+                num_1,
+                num_2,
+                operator,
+            } => {
+                let val_1 = num_1._eval();
+                let val_2 = num_2._eval();
+
+                match *operator {
+                    "+" => val_1 + val_2,
+                    "-" => val_1 - val_2,
+                    "*" => val_1 * val_2,
+                    "/" => val_1 / val_2,
+                    _ => unreachable!(),
+                }
+            }
+        }
+    }
+
+    fn eval(&self) -> f64 {
+        self._eval().try_into().unwrap()
+    }
+}
+
+impl fmt::Display for NumericExpression {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            NumericExpression::Number(n) => write!(f, "{}", n),
+            NumericExpression::Expression {
+                num_1,
+                num_2,
+                operator,
+            } => write!(f, "({} {} {})", num_1, operator, num_2),
+        }
+    }
+}
+
+impl hash::Hash for NumericExpression {
+    fn hash<H: hash::Hasher>(&self, state: &mut H) {
+        format!("{}", self).hash(state);
+    }
+}
+
+impl PartialEq for NumericExpression {
+    fn eq(&self, other: &Self) -> bool {
+        format!("{}", self) == format!("{}", other)
+    }
+}
+
+impl Eq for NumericExpression {}
+
+pub fn find_solutions(cardset: Vec<NumericExpression>) -> Vec<NumericExpression> {
+    if cardset.len() == 1 {
+        return {
+            if cardset.first().unwrap().eval() == TARGET_NUM as f64 {
+                vec![cardset.first().unwrap().clone()]
+            } else {
+                Vec::new()
+            }
+        };
+    }
+
+    let mut solutions = HashSet::new();
+
+    for idx_1 in 0..cardset.len() {
+        for idx_2 in 0..cardset.len() {
+            if idx_1 == idx_2 {
+                continue;
+            }
+
+            let num_1 = &cardset[idx_1];
+            let num_2 = &cardset[idx_2];
+            let mut new_cardset: Vec<_> = cardset
+                .iter()
+                .enumerate()
+                .filter_map(|(i, c)| {
+                    if i != idx_1 && i != idx_2 {
+                        Some(c.clone())
+                    } else {
+                        None
+                    }
+                })
+                .collect();
+
+            for operator in ["+", "-", "*", "/"] {
+                // enforce +, -, * to have to have big_num op small_num
+                if (operator == "+" || operator == "-" || operator == "*")
+                    && (num_1.eval() < num_2.eval())
+                {
+                    continue;
+                }
+
+                new_cardset.push(NumericExpression::Expression {
+                    num_1: Box::new(num_1.clone()),
+                    num_2: Box::new(num_2.clone()),
+                    operator,
+                });
+                solutions.extend(find_solutions(new_cardset.clone()));
+                new_cardset.pop();
+            }
+        }
+    }
+
+    solutions.into_iter().collect()
+}
+
+pub fn generate_cardset() -> (Vec<u64>, Vec<NumericExpression>) {
+    let mut cardset;
+    let mut solutions;
+
+    while {
+        cardset = Vec::new();
+
+        for _ in 0..NUM_CARDS {
+            let rand_number = rand::thread_rng().gen_range(1..=MAX_CARD_VALUE);
+            cardset.push(rand_number);
+        }
+
+        solutions = find_solutions(
+            cardset
+                .iter()
+                .map(|c| NumericExpression::Number(*c))
+                .collect(),
+        );
+
+        solutions.is_empty()
+    } {}
+
+    (cardset, solutions)
+}
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,30 @@
 extern crate rocket;
 
 use minijinja::{context, path_loader, Environment};
-use rocket::{response::content::RawHtml, State};
+use rocket::{response::content::RawHtml, serde::json::Json, State};
+use serde::Serialize;
+mod generator;
+
+#[derive(Serialize)]
+struct Cards {
+    cards: Vec<u64>,
+    solutions: Vec<String>,
+}
+
+#[get("/generate_cards", format = "json")]
+fn generate_cards_route() -> Json<Cards> {
+    let (cardset, solutions) = generator::generate_cardset();
+
+    let cards = Cards {
+        cards: cardset,
+        solutions: solutions
+            .iter()
+            .map(|c| format!("{}", c))
+            .collect::<Vec<_>>(),
+    };
+
+    Json(cards)
+}
 
 #[get("/hello")]
 fn world(jinja_env: &State<Environment>) -> RawHtml<String> {
@@ -10,7 +33,7 @@ fn world(jinja_env: &State<Environment>) -> RawHtml<String> {
         jinja_env
             .get_template("index.html")
             .and_then(|t| t.render(context! {}))
-            .unwrap()
+            .unwrap(),
     )
 }
 
@@ -22,5 +45,7 @@ fn run() -> _ {
         env
     };
 
-    rocket::build().mount("/", routes![world]).manage(jinja_env)
+    rocket::build()
+        .mount("/", routes![world, generate_cards_route])
+        .manage(jinja_env)
 }
